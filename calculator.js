a178e5e58fa112e34f484585e5fc3ab72b2b472c
Noura Al-Obaidly <Engineer.Nuwiin@Nouras-MacBook.local>
2020-06-16 17:41:01 +0300

    Conditional Task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 // Define the numbers and the operation here
 
 // Write if-statements here
+const FirstVar = 3;
+const SecondVar = 5;
+const operation1 = "+";
+const operation2 = "-";
+const operation3 = "*";
+const operation4 = "/";
+
+if (operation1 == "+") {
+  console.log(`${FirstVar} + ${SecondVar} = ${FirstVar + SecondVar}`);
+} else if ((operation2 = "-")) {
+  console.log(`${FirstVar} - ${SecondVar} = ${FirstVar - SecondVar}`);
+} else if (operation3 == "*") {
+  console.log(`${FirstVar} * ${SecondVar} = ${FirstVar * SecondVar}`);
+} else if (operation4 == "/") {
+  console.log(`${FirstVar} / ${SecondVar} = ${FirstVar / SecondVar}`);
+}
